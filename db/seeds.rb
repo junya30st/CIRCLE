f8d1636762b0821d10dd982bafc60f2181872dfb
junya30st <ji06140210@gmail.com>
2020-01-19 12:51:03 +0900

    create-category-page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create!(
-   name: '開発',
-)
+Category.first_or_create([
+   { id: 1, value: 'ブログ' },
+   { id: 2, value: '開発' },
+   { id: 3, value: 'Vue.js' },
+   { id: 4, value: 'Ruby' },
+   { id: 5, value: 'Rails' },
+   { id: 6, value: 'Python' },
+   { id: 7, value: 'JavaScript' },
+   { id: 8, value: 'jQuery' },
+   { id: 9, value: 'HTML' },
+   { id: 10, value: 'CSS' },
+   { id: 11, value: 'AWS' },
+])
