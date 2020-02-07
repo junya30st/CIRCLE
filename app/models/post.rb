3de2a3087b66d710aa0dc98fc4f0700c041f40fa
junya30st <ji06140210@gmail.com>
2020-02-07 19:16:14 +0900

    create-likes_view-edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :likes
 
   has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
   has_many :posts
+  has_many :likes
 
   has_many :messages
   
