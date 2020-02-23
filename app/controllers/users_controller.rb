9434947636c07bb5a05789584fd11702d8d3478f
junya30st <ji06140210@gmail.com>
2020-02-23 22:45:31 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
 def show
   @user = User.find(params[:id])
   @post = Post.where(user_id: @user.id).order('id DESC')
-  @posts = Post.find(params[:id])
 end
 
 end
